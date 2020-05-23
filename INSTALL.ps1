5e149723143f6ae559aba85596f81ad462c0ba1a
Nicholas Stevens <nstevens1040@Yahoo.com>
2020-05-23 09:25:27 -0500

    Update INSTALL.ps1

--- INSTALL.ps1
+++ INSTALL.ps1
@@ -104,8 +104,18 @@ function Execute-WebRequest {
             [string]$AssemblyName
         )
         $SDIR = "$($PWD.Path)"
-        if([System.IO.Directory]::GetFiles("C:\Windows\Microsoft.Net\assembly\GAC_MSIL","*$($AssemblyName).dll",[System.IO.SearchOption]::AllDirectories)){
-            Add-Type -Path "$([System.IO.Directory]::GetFiles("C:\Windows\Microsoft.Net\assembly\GAC_MSIL","*$($AssemblyName).dll",[System.IO.SearchOption]::AllDirectories))"
+        if(
+            [System.IO.Directory]::GetFiles("C:\Windows\Microsoft.Net\assembly\GAC_MSIL","*$($AssemblyName).dll",[System.IO.SearchOption]::AllDirectories) -or `
+            "$([System.IO.Directory]::GetDirectories("$($PWD.Path)","*$($AssemblyName)*",[System.IO.SearchOption]::AllDirectories))\lib"
+        ){
+            if([System.IO.Directory]::GetFiles("C:\Windows\Microsoft.Net\assembly\GAC_MSIL","*$($AssemblyName).dll",[System.IO.SearchOption]::AllDirectories)){
+                Add-Type -Path "$([System.IO.Directory]::GetFiles("C:\Windows\Microsoft.Net\assembly\GAC_MSIL","*$($AssemblyName).dll",[System.IO.SearchOption]::AllDirectories))"
+            }
+            if("$([System.IO.Directory]::GetDirectories("$($PWD.Path)","*$($AssemblyName)*",[System.IO.SearchOption]::AllDirectories))\lib"){
+                cd "$([System.IO.Directory]::GetDirectories("$($PWD.Path)","*$($AssemblyName)*",[System.IO.SearchOption]::AllDirectories))\lib"
+                cd "$([System.IO.Directory]::GetDirectories("$($PWD.Path)","net??",[System.IO.SearchOption]::AllDirectories) | sort | select -Last 1)"
+                Add-Type -Path "$([System.io.Directory]::GetFiles("$($PWD.Path)","*.dll"))"
+            }
         } else {
             if(![system.io.file]::Exists("C:\ProgramData\chocolatey\bin\choco.exe")){
                 $p = [system.Diagnostics.Process]@{
